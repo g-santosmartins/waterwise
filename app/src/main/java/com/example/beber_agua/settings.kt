79e668383172cb71363fb9c7e9e7d757fa88615c
Guilherme Martins <g.santosmartins@outlook.com>
2022-11-27 20:54:23 -0300

    feat: finished app layout

---
+++ app/src/main/java/com/example/beber_agua/settings.kt
@@ -0,0 +1,11 @@
+package com.example.beber_agua
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class settings : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
