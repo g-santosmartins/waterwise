7c921aad2130edd3a44ae8fca989ac550673c38c
Guilherme Martins <g.santosmartins@outlook.com>
2022-12-09 13:54:25 -0300

    refactor: how much water class util

--- app/src/main/java/com/example/beber_agua/utils/CalculateHowMuchWater.kt
+++ app/src/main/java/com/example/beber_agua/utils/CalculateHowMuchWater.kt
@@ -3,17 +3,14 @@ package com.example.beber_agua.utils
 class CalculateHowMuchWater {
 
     fun calculate(weight: Double, age: Int): Double {
-
-        var result: Double
-        when (age) {
-            in 0..17 -> result = (weight * 40 / 100 / 10)
-            in 18..55 -> result = (weight * 35 / 100 / 10)
-            in 56..65 -> result = (weight * 30 / 100 / 10)
-            in 66..150 -> result = (weight * 25 / 100 / 10)
+        return when (age) {
+            in 0..17 -> (weight * 40 / 100 / 10)
+            in 18..55 -> (weight * 35 / 100 / 10)
+            in 56..65 -> (weight * 30 / 100 / 10)
+            in 66..150 -> (weight * 25 / 100 / 10)
             else -> {
-                result = 0.0
+                0.0
             }
         }
-        return result
     }
 }
